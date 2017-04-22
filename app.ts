76ad690406376d0c955e6f48b7e8ea5cfeee9a55
Jesse Pinho <jesse@jessepinho.com>
2017-04-22 20:35:29 +0200

    refactor: remove Express and make a super simple proxy server

--- app.ts
+++ app.ts
@@ -1,15 +1,12 @@
-import * as http from 'http';
 import * as httpProxy from 'http-proxy';
 
-const proxy = httpProxy.createProxyServer({ changeOrigin: true });
-proxy.on('proxyReq', function(proxyReq) {
-  proxyReq.setHeader('Authorization', `Bearer ${process.env.TWITTER_BEARER_TOKEN}`);
-});
-
-proxy.on('proxyRes', function(proxyRes, req, res) {
-  console.log('##### GOT RESPONSE!', res.body);
-});
-
-http.createServer(function(req, res) {
-  proxy.web(req, res, { target: 'https://api.twitter.com' });
-}).listen(3000);
+const proxy = httpProxy
+  .createProxyServer({
+    changeOrigin: true,
+    headers: {
+      Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+    },
+    preserveHeaderKeyCase: true,
+    target: 'https://api.twitter.com',
+  })
+  .listen(3000);
