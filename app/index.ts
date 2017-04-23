42a732486c5d6663c07270b505fb0cd4826bb099
Jesse Pinho <jesse@jessepinho.com>
2017-04-23 17:03:54 +0200

    feat: use Express to restrict routes; move app into /app

---
+++ app/index.ts
@@ -0,0 +1,23 @@
+import * as express from 'express';
+import * as httpProxy from 'http-proxy';
+
+const app = express();
+
+const proxy = httpProxy
+  .createProxyServer({
+    changeOrigin: true,
+    headers: {
+      Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+    },
+    target: 'https://api.twitter.com',
+  });
+
+app.get('/1.1/statuses/user_timeline.json', function(req, res) {
+  if (req.query.screen_name === 'realDonaldTrump') {
+    proxy.web(req, res);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.listen(process.env.PORT || 3000);
