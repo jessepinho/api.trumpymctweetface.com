687de453d625cc9eb6751d1800ab11c250886164
Jesse Pinho <jesse@jessepinho.com>
2018-03-18 20:45:29 +0100

    chore: move the Express app into functions/

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,35 @@
-import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as httpProxy from 'http-proxy';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+const proxy = httpProxy
+  .createProxyServer({
+    changeOrigin: true,
+    headers: {
+      Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+    },
+    target: 'https://api.twitter.com',
+  });
+
+proxy.on('proxyRes', function(proxyRes, req, res) {
+  if (process.env.NODE_ENV === 'development') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', 'http://trumpymctweetface.com');
+  }
+});
+
+app.get('/1.1/statuses/user_timeline.json', function(req, res) {
+  if (req.query.screen_name === 'realDonaldTrump') {
+    proxy.web(req, res);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.get('/1.1/statuses/show.json', function(req, res) {
+  proxy.web(req, res);
+});
+
+app.listen(process.env.PORT || 5000);
